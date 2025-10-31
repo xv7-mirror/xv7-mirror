6baa94386cc91ddca4cc05bf3f12d04385e9069e
vlprokopenko <vlprokopenko@disroot.org>
2025-10-31 20:10:12 +0200

    init: improvements
    - Log entire (almost) init process into
    initlog.txt
    
    - More useful output when reaping a zombie
    
    - Added a very quick delay in order to
    prevent crash loops
    
    Signed-off-by: vlprokopenko <vlprokopenko@disroot.org>

--- userspace/bin/init.c
+++ userspace/bin/init.c
@@ -12,26 +12,51 @@ main(void)
 {
   int pid, wpid;
 
-  if(open("console", O_RDWR) < 0){
+  /*
+   * Create the console device.
+   */
+  int fd = open("console", O_RDWR);
+  if(fd < 0){
     mknod("console", 1, 1);
-    open("console", O_RDWR);
+    fd = open("console", O_RDWR);
   }
-  dup(0);  // stdout
-  dup(0);  // stderr
+
+  if(fd < 0){
+    printf(2, "init: cannot open console\n");
+    exit();
+  }
+
+  dup(fd);  // stdout
+  dup(fd);  // stderr
+
+  /*
+   * Initially try to open initlog.txt as RDWR,
+   * and if it fails create it.
+   */
+  int logfd = open("/etc/initlog.txt", O_RDWR);
+
+  if(logfd < 0){
+    logfd = open("/etc/initlog.txt", O_CREATE | O_RDWR);
+    printf(logfd, "init: no /etc/initlog.txt, creating\n");
+  }
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(logfd, "init: starting sh\n");
     pid = fork();
     if(pid < 0){
-      printf(1, "init: fork failed\n");
+      printf(logfd, "init: fork failed\n");
       exit();
     }
+
     if(pid == 0){
       exec("/bin/sh", argv);
-      printf(1, "init: exec sh failed\n");
+      printf(logfd, "init: exec /bin/sh failed\n");
       exit();
     }
     while((wpid=wait()) >= 0 && wpid != pid)
-      printf(1, "zombie!\n");
-  }
+      printf(logfd, "init: reaped zombie %d\n", wpid);
+    }
+
+    /* Quick delay to prevent crash loops */
+    sleep(1);
 }
