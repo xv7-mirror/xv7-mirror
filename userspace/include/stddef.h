1e047e6ae7c9150c463d1a12cadce62f95f0e686
NerdNextDoor <NerdNextDoor@noreply.codeberg.org>
2025-11-01 21:53:00 +0000

    gitignore: ignore cp'd includes and the libc

--- userspace/include/stddef.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDDEF_H_
-#define _STDDEF_H_
-
-typedef unsigned int size_t;
-typedef int ssize_t;
-#define NULL ((void*)0)
-
-#endif
--- userspace/include/stdint.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDINT_H_
-#define _STDINT_H_
-
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
-typedef unsigned int uintptr_t;
-
-#endif
