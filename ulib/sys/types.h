9eff12c0c6fdbc65a70489d6d313dd39654cfd6e
vlprokopenko <vlprokopenko@disroot.org>
2025-11-02 14:51:22 +0200

    ulib: actually no we don't need these yet
    Signed-off-by: vlprokopenko <vlprokopenko@disroot.org>

--- ulib/sys/types.h
+++ ulib/sys/types.h
@@ -4,10 +4,4 @@
 typedef unsigned int uint;
 typedef unsigned short ushort;
 typedef unsigned char uchar;
-typedef uint dev_t;
-typedef uint ino_t;
-typedef uint mode_t;
-typedef uint nlink_t;
-typedef uint off_t;
-
 #endif
