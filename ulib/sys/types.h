ade69086212e1b963427b0d59d62b67ac54d3be4
vlprokopenko <vlprokopenko@disroot.org>
2025-11-02 14:50:53 +0200

    implement sys/types.h
    Signed-off-by: vlprokopenko <vlprokopenko@disroot.org>

---
+++ ulib/sys/types.h
@@ -0,0 +1,13 @@
+#ifdef _SYS_TYPES_H_
+#define _SYS_TYPES_H_
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+typedef uint dev_t;
+typedef uint ino_t;
+typedef uint mode_t;
+typedef uint nlink_t;
+typedef uint off_t;
+
+#endif
